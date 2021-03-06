7a04d90f23732fd04d57473406223f0b9fc54b55
Павел Мочалов <78229095+010MochalovPA@users.noreply.github.com>
2021-03-06 19:47:11 +0300

    fix: fix mistake

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,6 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    let check = parseFloat(sampleActivity);
-    if (sampleActivity < 0 || !sampleActivity ||  MODERN_ACTIVITY < sampleActivity || !(check) || typeof sampleActivity != 'string') return false;    
+    if (sampleActivity < 0 || !sampleActivity ||  MODERN_ACTIVITY < sampleActivity || !(parseFloat(sampleActivity)) || typeof sampleActivity != 'string') return false;    
     return Math.ceil( Math.log( MODERN_ACTIVITY / sampleActivity) / (Math.log(2) / HALF_LIFE_PERIOD));
 };
