419baa41130883b915f35a8fe745d973181ad62b
Павел Мочалов <78229095+010MochalovPA@users.noreply.github.com>
2021-03-07 16:26:14 +0300

    feat: add function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!arr) throw new Error('arr is not an Array');
+    let result = arr.slice();
+    if (result[0] === '--discard-prev' || result[0] === '--double-prev') result.shift();
+    if (result[result.length - 1] === '--discard-next' || result[result.length - 1] === '--double-next') result.pop();  
+    for (let i = 0; i < result.length; i++ ) {
+        if (result[i].toString() === '--discard-next' ) result.splice(i, 2, "deletedItem"); 
+        else if (result[i].toString() === '--discard-prev') result.splice(i - 1, 2, "deletedItem");
+        else if (result[i].toString() === '--double-next') result.splice(i, 1, result[i + 1]);
+        else if (result[i].toString() === '--double-prev' ) result.splice(i, 1, result[i - 1]);
+    }
+    return result.filter(item => item != 'deletedItem');
 };
