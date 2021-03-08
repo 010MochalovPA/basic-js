e5fe4975d105ce6baa1fcdc5a1549f009ceb8f54
Павел Мочалов <78229095+010MochalovPA@users.noreply.github.com>
2021-03-08 13:43:21 +0300

    feat: add methods encrypt and decrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,62 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  alphavite = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  isNormal = true;
+  constructor (isNormal) {
+    if (isNormal === false) this.isNormal = false;
+    else this.isNormal = true;
+  }
+  encrypt(message,key) {
+    if (!message || !key) throw new Error('Error');
+    let result = [];
+    let entryMessage = message.split('');
+    let entryKey = key
+                      .repeat(Math.ceil(message.length / key.length))
+                      .substring(0,message.length)
+                      .split('');
+    for (let i = 0;i < entryMessage.length;i++){
+      if (!~this.alphavite.indexOf(entryMessage[i].toUpperCase())){
+        entryKey.splice(i,0," ");
+        entryKey.pop();
+      }
+    }
+    for (let i = 0; i < entryMessage.length; i++){
+      if (!~this.alphavite.indexOf(entryMessage[i].toUpperCase())) {
+        result[i] = entryMessage[i];
+        continue;
+      }
+      result[i] =  this.alphavite[(+this.alphavite.indexOf(entryMessage[i].toUpperCase()) + +this.alphavite.indexOf(entryKey[i].toUpperCase()))%26];
+    }
+    if (this.isNormal){
+      return result.join('')
+    } else return result.reverse().join('');
+    
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage,key) {
+    if (!encryptedMessage || !key) throw new Error('Error');
+    let result = [];
+    let entryMessage = encryptedMessage.split('');
+    let entryKey = key
+                      .repeat(Math.ceil(encryptedMessage.length / key.length))
+                      .substring(0,encryptedMessage.length)
+                      .split('');
+    for (let i = 0;i < entryMessage.length;i++){
+      if (!~this.alphavite.indexOf(entryMessage[i].toUpperCase())){
+        entryKey.splice(i,0," ");
+        entryKey.pop();
+      }
+    }
+    for (let i = 0; i < entryMessage.length; i++){
+      if (!~this.alphavite.indexOf(entryMessage[i].toUpperCase())) {
+        result[i] = entryMessage[i];
+        continue;
+      }
+      result[i] =  this.alphavite[(+this.alphavite.indexOf(entryMessage[i].toUpperCase()) - +this.alphavite.indexOf(entryKey[i].toUpperCase())+ 26 ) % 26];
+    }
+    if (this.isNormal){
+      return result.join('')
+    } else return result.reverse().join('');
   }
 }
 
